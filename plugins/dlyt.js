e1b453dd75a4699c457b8081737eee08337e1fb6
XnodesDev <97332906+xnodesdev@users.noreply.github.com>
2025-01-30 15:39:34 +0530

    ＧＡＲＦＩＥＬＤ ｖ10 🧬

--- plugins/dlyt.js
+++ plugins/dlyt.js
@@ -22,15 +22,13 @@ async (conn, mek, m, { from, prefix, quoted, q, reply, waitForReply }) => {
 
         let yts = yt.results[0];
 
-        let ytmsg = `*𝖦Λ𝖱𝖥𝖨Ξ𝖫𝖣 𝖡𝖮Т 𝖵𝖨𝖣𝖤𝖮 𝖣𝖮𝖶𝖭𝖫𝖮𝖠𝖣𝖤𝖱*
-
-🎥 *𝖵𝖨𝖣𝖤𝖮 𝖣𝖮𝖶𝖭𝖫𝖮𝖠𝖣𝖤𝖱*
-
+        let ytmsg = `🎥 *𝖵𝖨𝖣𝖤𝖮 𝖣𝖮𝖶𝖭𝖫𝖮𝖠𝖣𝖤𝖱*
 🎬 *Title* -  ${yts.title}
 ⏳ *Duration* - ${yts.timestamp}
 👁️ *Views* -  ${yts.views}
 👤 *Author* -  ${yts.author.name}
-🔗 *Link* -  ${yts.url}`;
+🔗 *Link* -  ${yts.url}
+> 𝖦Λ𝖱𝖥𝖨Ξ𝖫𝖣 𝖡𝖮Т`;
 
         // Send video details
         await conn.sendMessage(from, { image: { url: yts.thumbnail || yts.image || '' }, caption: `${ytmsg}` }, { quoted: mek });
@@ -43,7 +41,7 @@ async (conn, mek, m, { from, prefix, quoted, q, reply, waitForReply }) => {
         await conn.sendMessage(from, {
             video: { url: ytdl.download.url },
             mimetype: "video/mp4",
-            caption: `> *${yts.title}*\n> *Quality: ${quality}*\n> 𝖦Λ𝖱𝖥𝖨Ξ𝖫𝖣 𝖡𝖮Т`
+            caption: `> *${yts.title}*\n> *Quality: ${quality}*\n> *© Pᴏᴡᴇʀᴇᴅ Bʏ Mʀ Sʜᴀʙᴀɴ ♡*`
         }, { quoted: mek });
     } catch (e) {
         console.log(e);
@@ -62,40 +60,33 @@ cmd({
     use: '.song < Yt url or Name >',
     filename: __filename
 },
-async(conn, mek, m, { from, prefix, quoted, q, reply }) => {
-try {
+async(conn, mek, m,{ from, prefix, quoted, q, reply }) => {
+try{
 
 if(!q) return await reply("Please give me Yt url or Name")
 	
-const yt = await ytsearch(q);
-if(yt.results.length < 1) return reply("Results not found!")
+const yt = await ytsearch(q); 
+if(yt.results.length < 1) return reply("Results is not found !")
 
-let yts = yt.results[0];  
+let yts = yt.results[0]  
 const ytdl = await ytmp3(yts.url, { quality: 'highestaudio' });
-
-let ytmsg = `*𝖦Λ𝖱𝖥𝖨Ξ𝖫𝖣 𝖣𝖮𝖶𝖭𝖫𝖮𝖠𝖣𝖤𝖱*
-
+		
+let ytmsg = `
 🎵 *𝖬𝖴𝖲𝖨𝖢 𝖣𝖮𝖶𝖭𝖫𝖮𝖠𝖣𝖤𝖱*
-
 🎬 *Title* - ${yts.title}
 ⏳ *Duration* - ${yts.timestamp}
 👁️ *Views* - ${yts.views}
 👤 *Author* - ${yts.author.name}
-🔗 *Link* - ${yts.url}`;
-
+🔗 *Link* - ${yts.url}
+> 𝖦Λ𝖱𝖥𝖨Ξ𝖫𝖣 𝖡𝖮Т`
 // SEND DETAILS
-await conn.sendMessage(from, { image: { url: yts.thumbnail || yts.image || '' }, caption: `${ytmsg}` }, { quoted: mek });
+await conn.sendMessage(from, { image: { url: yts.thumbnail || yts.image || '' }, caption: `${ytmsg}`}, { quoted: mek });
 
-} catch (error) {
-    console.error(error);
-    await reply("An error occurred while processing your request.");
-}
-});
 // SEND AUDIO TYPE
 await conn.sendMessage(from, { audio: { url: ytdl.download.url }, mimetype: "audio/mpeg" }, { quoted: mek })
 
 // SEND DOC TYPE
-await conn.sendMessage(from, { document: { url: ytdl.download.url }, mimetype: "audio/mpeg", fileName: ytdl.result.title + '.wav', caption: `> *𝖦Λ𝖱𝖥𝖨Ξ𝖫𝖣 𝖡𝖮Т*` }, { quoted: mek })
+await conn.sendMessage(from, { document: { url: ytdl.download.url }, mimetype: "audio/mpeg", fileName: ytdl.result.title + '.mp3', caption: `> 𝖦Λ𝖱𝖥𝖨Ξ𝖫𝖣 𝖡𝖮Т` }, { quoted: mek })
 
 
 } catch (e) {
