0d3205956a928e1e0bc6f578d7b50c540b1d5cd8
XnodesDev <97332906+xnodesdev@users.noreply.github.com>
2025-02-01 20:27:45 +0530

    ＧＡＲＦＩＥＬＤ ＢＯＴ ｖ10 🧬

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
     SESSION_ID: "Xnodes~e2R2VQgS#KFvTRMN77ZGFOYSKbA4mpNjwt7KBwi5pRFwRoG1-vi4", // add your Session Id  
     AUTO_STATUS_SEEN: true,  // set to true or false for auto-seeing status
-    AUTO_STATUS_REPLY: false,  // set to true if you want auto-reply on status
+    AUTO_STATUS_REPLY: true,  // set to true if you want auto-reply on status
     AUTO_STATUS_REACT: true,  // set to true if you want auto-react on status
     AUTO_STATUS_MSG: "GARFIELD-WHATSAPP-BOT-v10",  // set the auto-reply message for status replies
     PREFIX: ".",  // add your prefix for the bot
     BOT_NAME: "𝖦Λ𝖱𝖥𝖨Ξ𝖫𝖣 𝖡𝖮Т",  // add bot name for the menu
     STICKER_NAME: "GARFIELD-WHATSAPP-BOT-v10",  // set the sticker pack name
-    CUSTOM_REACT: true,  // set to true for custom emoji reactions
+    CUSTOM_REACT: false,  // set to true for custom emoji reactions
     CUSTOM_REACT_EMOJIS: "⭐",  // choose custom react emojis
     DELETE_LINKS: false,  // set to true for automatic deletion of links without removing member
     OWNER_NUMBER: "94711502119",  // add your bot owner number
@@ -21,8 +21,8 @@ module.exports = {
     DESCRIPTION: "©Ｐｏｗｅｒｅｄ ｂｙ Ｘｎｏｄｅｓ",  // add bot description
     ALIVE_IMG: "https://i.ibb.co/5g7VGhC9/Picsart-25-01-30-13-20-52-736.png",  // add image URL for alive message
     LIVE_MSG: "> 𝖢𝗈𝖽𝖾𝖽 𝖻𝗒 𝖳𝗁𝖺𝗋𝗂𝗇𝖽𝗎 𝖫𝗂𝗒𝖺𝗇𝖺𝗀𝖾",  // add alive message
-    READ_MESSAGE: false,  // set to true or false for automatic reading of messages
-    AUTO_REACT: false,  // set to true or false for auto-reactions on all messages
+    READ_MESSAGE: true,  // set to true or false for automatic reading of messages
+    AUTO_REACT: true,  // set to true or false for auto-reactions on all messages
     ANTI_BAD: false,  // set to true for anti-bad words filter
     MODE: "public",  // set the bot mode to public, private, inbox, or group
     ANTI_LINK: false,  // set to true for anti-link protection in groups
