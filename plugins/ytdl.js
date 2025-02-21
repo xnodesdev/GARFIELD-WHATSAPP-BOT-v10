93881a604bf571e2cf67c456a96c6d431e3762f4
XnodesDev <97332906+xnodesdev@users.noreply.github.com>
2025-02-21 10:06:39 +0530

    ＧＡＲＦＩＥＬＤ ＢＯＴ ｖ10 🧬

--- plugins/ytdl.js
+++ plugins/ytdl.js
@@ -50,7 +50,7 @@ cmd(
         );
       }
 
-      reply("🔍 Searching for the song... 🎵");
+      reply("```🔍 Searching for the song... 🎵```");
 
       // Search for the song using yt-search with rate limiting
       const searchResults = await limiter.schedule(() => yts(searchQuery));
@@ -129,7 +129,7 @@ cmd(
         );
       }
 
-      reply("🔍 Searching for the video... 🎥");
+      reply("```🔍 Searching for the video... 🎥```");
 
       // Search for the video using yt-search with rate limiting
       const searchResults = await limiter.schedule(() => yts(searchQuery));
