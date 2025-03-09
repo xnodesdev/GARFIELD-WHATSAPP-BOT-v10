cd3e3f7887de7536bcb9d28f1b9225afbd189372
XnodesDev <97332906+xnodesdev@users.noreply.github.com>
2025-03-09 09:43:31 +0530

    ＧＡＲＦＩＥＬＤ ＢＯＴ ｖ10 🧬

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 } 
  
 module.exports = {
-    SESSION_ID: "Xnodes~imJlFJrQ#me_HO7YNBg6qChD5PW8SxItq3tt88Wrv4lDsSMuG1Bw", // add your Session Id  Ex : Xnodes~e2R2VQgS#KFvTRMN77ZGFOYSKbA4mpNjwt7KBwiRFwRoG1-vi4"
+    SESSION_ID: "Xnodes~OnhCFByb#75Pc7LFB9eJepmx3su18s8eMjlar8pVJ2hflf1-tTQg", // add your Session Id  Ex : Xnodes~e2R2VQgS#KFvTRMN77ZGFOYSKbA4mpNjwt7KBwiRFwRoG1-vi4"
     AUTO_STATUS_SEEN: true,  // set to true or false for auto-seeing status
     AUTO_STATUS_REPLY: true,  // set to true if you want auto-reply on status
     AUTO_STATUS_REACT: true,  // set to true if you want auto-react on status
